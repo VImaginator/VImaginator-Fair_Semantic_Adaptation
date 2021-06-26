9664db690fc7ffad63b83cf8f3f103ad66e81114
VImaginator <vimaginator@proton.me>
2021-06-26 07:14:09 +0000

    update api

--- inference.py
+++ inference.py
@@ -16,3 +16,21 @@ if __name__ == "__main__":
     parser.add_argument("--input_path", type = str)
     parser.add_argument("--output_path", type = str)
     parser.add_argument("--alpha", type=float, default=0.5)
+    args = parser.parse_args()
+
+    model = DeepLabV2_ResNet101(num_classes = 19)
+    checkpoint = torch.load(args.checkpoint)
+    msg = model.load_state_dict(checkpoint)
+    print(f"Loading model from {args.checkpoint}: {msg}")
+
+    transform = T.Compose([
+        T.ToTensor(),
+        T.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+    ])
+
+    image = Image.open(args.input_path)
+    image = image.convert("RGB")
+    (width, height) = (image.width // 2, image.height // 2) # Save memory of GPU
+    image = image.resize((width, height))
+    image = transform(image)
+    image = image.unsqueeze(0)
