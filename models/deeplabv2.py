f97baf08603f5497d69074ac0fe5761cc9226f8a
VImaginator <vimaginator@proton.me>
2021-10-06 12:20:13 +0000

    nuget update

--- models/deeplabv2.py
+++ models/deeplabv2.py
@@ -15,4 +15,5 @@ BatchNorm = nn.BatchNorm2d #nn.SyncBatchNorm
 
 def conv3x3(in_planes, out_planes, stride=1):
     "3x3 convolution with padding"
-    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=st
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+          
