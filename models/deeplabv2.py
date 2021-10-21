03b8c0df019d43b9ef3dd37caeaa224d22b71b54
VImaginator <vimaginator@proton.me>
2021-10-21 13:43:16 +0000

    Version 0.1.4-SNAPSHOT

--- models/deeplabv2.py
+++ models/deeplabv2.py
@@ -18,4 +18,4 @@ def conv3x3(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
                      padding=1, bias=False)
 
-class
+class BasicBlock(nn
