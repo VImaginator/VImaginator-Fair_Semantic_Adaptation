8bd5e7f26ea4e5d7747907dfc6d4ff417f6a17fc
VImaginator <vimaginator@proton.me>
2021-12-13 14:56:02 +0000

    Bumped dependencies to the latest version

--- models/deeplabv2.py
+++ models/deeplabv2.py
@@ -27,3 +27,4 @@ class BasicBlock(nn.Module):
         self.bn1 = BatchNorm(planes, affine = affine_par)
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = conv3x3(planes, planes)
+        self.bn2 =
