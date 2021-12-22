7cd1682ea99f11ff926ee4c5e3557e696af9cdd8
VImaginator <vimaginator@proton.me>
2021-12-22 09:56:09 +0000

    More content

--- models/deeplabv2.py
+++ models/deeplabv2.py
@@ -27,4 +27,4 @@ class BasicBlock(nn.Module):
         self.bn1 = BatchNorm(planes, affine = affine_par)
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = conv3x3(planes, planes)
-        self.bn2 =
+        self.bn2 = BatchNorm(plane
