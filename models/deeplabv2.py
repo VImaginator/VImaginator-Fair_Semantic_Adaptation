ba23476196edb1e978fef1d666b24e6b1fbfa10d
VImaginator <vimaginator@proton.me>
2022-03-28 16:31:41 +0000

    pub

--- models/deeplabv2.py
+++ models/deeplabv2.py
@@ -62,4 +62,4 @@ class Bottleneck(nn.Module):
 
         padding = dilation
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, # change
-                               padding=padding, bias=False, dilatio
+                               padding=padding, bias=False, dilation = dilatio
