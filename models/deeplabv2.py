6dc9a5abba98ac661bdb90fc0b8927d3501e2937
VImaginator <vimaginator@proton.me>
2022-04-20 15:43:51 +0000

    Change footer

--- models/deeplabv2.py
+++ models/deeplabv2.py
@@ -67,4 +67,4 @@ class Bottleneck(nn.Module):
         #for i in self.bn2.parameters():
         #    i.requires_grad = False
         self.conv3 = nn.Conv2d(planes, planes * 4, kernel_size=1, bias=False)
-        self
+        self.bn3 = BatchNorm(pl
