ffebb7e47e20ab82b9ab53015ffad642016db8b0
VImaginator <vimaginator@proton.me>
2022-07-12 06:32:14 +0000

    frameworks

--- models/deeplabv2.py
+++ models/deeplabv2.py
@@ -99,4 +99,4 @@ class Bottleneck(nn.Module):
 
 class Classifier_Module(nn.Module):
 
-    def __init__(self, fan_in, dilation_series, pad
+    def __init__(self, fan_in, dilation_series, padding_series,
