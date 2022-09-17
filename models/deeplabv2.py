dd339ce1386501b49a2dc3480b8f6f02712a6d80
VImaginator <vimaginator@proton.me>
2022-09-17 18:33:08 +0000

    Update forum.html

--- models/deeplabv2.py
+++ models/deeplabv2.py
@@ -119,4 +119,4 @@ class ResNet(nn.Module):
         self.inplanes = 64
         super(ResNet, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3,
-     
+                    
