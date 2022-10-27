d6f798f319aaec3e6dd603edfaf1a0c4327afac3
VImaginator <vimaginator@proton.me>
2022-10-27 21:49:36 +0000

    Add intro

--- models/deeplabv2.py
+++ models/deeplabv2.py
@@ -126,4 +126,4 @@ class ResNet(nn.Module):
 
         self.layer1 = self._make_layer(block, 64, layers[0])
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
-        self.layer3 = self._
+        self.layer3 = self._make_lay
