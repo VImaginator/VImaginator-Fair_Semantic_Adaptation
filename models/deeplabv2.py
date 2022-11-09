42ef30822d58bb2c2ce4ed8d1f0757e39d4aae84
VImaginator <vimaginator@proton.me>
2022-11-09 17:18:45 +0000

    re #483

--- models/deeplabv2.py
+++ models/deeplabv2.py
@@ -132,4 +132,5 @@ class ResNet(nn.Module):
         self.layer5 = Classifier_Module(2048, [6,12,18,24], [6,12,18,24], num_classes)
 
         for m in self.modules():
-            if isinstance(m, nn
+            if isinstance(m, nn.Conv2d):
+ 
