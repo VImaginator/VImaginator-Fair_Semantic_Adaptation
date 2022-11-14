84490f2eda0801680629b6ba2aa8aa5a0bfdd3dc
VImaginator <vimaginator@proton.me>
2022-11-14 15:10:57 +0000

    Delete .keep

--- models/deeplabv2.py
+++ models/deeplabv2.py
@@ -135,4 +135,4 @@ class ResNet(nn.Module):
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                 m.weight.data.normal_(0, 0.01)
-         
+            elif isinstance
