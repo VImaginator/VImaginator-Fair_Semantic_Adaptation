a97f1db0318b3a9e44323d9bbaa5040bdf3473e0
VImaginator <vimaginator@proton.me>
2022-11-23 17:22:26 +0000

    Update events.js

--- models/deeplabv2.py
+++ models/deeplabv2.py
@@ -141,4 +141,4 @@ class ResNet(nn.Module):
 
     def _make_layer(self, block, planes, blocks, stride=1, dilation=1):
         downsample = None
-        if stride != 1 o
+        if stride != 1 or self.inplanes !
