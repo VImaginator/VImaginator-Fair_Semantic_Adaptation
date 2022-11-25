d0864aa9f0046dba9376610133e87f2b76eb90fc
VImaginator <vimaginator@proton.me>
2022-11-25 21:07:09 +0000

    Include license

--- models/deeplabv2.py
+++ models/deeplabv2.py
@@ -141,4 +141,4 @@ class ResNet(nn.Module):
 
     def _make_layer(self, block, planes, blocks, stride=1, dilation=1):
         downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion or dilation
+        if stride != 1 or self.inplanes != planes * block.expansion or dilation == 2 or dil
