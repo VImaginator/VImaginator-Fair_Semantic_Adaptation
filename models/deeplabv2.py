a9b1cea663b972033bee368260cf15a696eb430c
VImaginator <vimaginator@proton.me>
2022-11-25 06:51:53 +0000

    auto update vendor

--- models/deeplabv2.py
+++ models/deeplabv2.py
@@ -141,4 +141,4 @@ class ResNet(nn.Module):
 
     def _make_layer(self, block, planes, blocks, stride=1, dilation=1):
         downsample = None
-        if stride != 1 or self.inplanes != planes * block.expansion or d
+        if stride != 1 or self.inplanes != planes * block.expansion or dilation
