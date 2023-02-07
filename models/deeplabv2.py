100aa3e5b769cf8a15a2a871c5c09252303e1a34
VImaginator <vimaginator@proton.me>
2023-02-07 08:07:58 +0000

    match

--- models/deeplabv2.py
+++ models/deeplabv2.py
@@ -178,4 +178,4 @@ class DeepLabV2_ResNet101(nn.Module):
         self.model = ResNet(Bottleneck, [3, 4, 23, 3], num_classes)
 
         # converting to SyncBatchNorm
-        #self.model = torch.nn.SyncBatchNorm.con
+        #self.model = torch.nn.SyncBatchNorm.convert_sync_b
